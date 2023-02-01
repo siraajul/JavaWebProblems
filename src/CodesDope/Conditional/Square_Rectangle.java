fdd691a0af39aae964b0efc8a640ecfbbd3e8618
itsirajul <shirajulislamparvez@gmail.com>
2023-02-01 22:54:31 +0600

    Check its Square or Rectangle(CodesDope)

--- src/CodesDope/Conditional/Square_Rectangle.java
+++ src/CodesDope/Conditional/Square_Rectangle.java
@@ -1,4 +1,23 @@
 package CodesDope.Conditional;
 
+import java.util.Scanner;
+
 public class Square_Rectangle {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Length: ");
+        int length = input.nextInt();
+        System.out.print("Enter the Widht: ");
+        int widht = input.nextInt();
+
+        if(length==widht){
+            System.out.println("Its a Square");
+        }
+        else {
+            System.out.println("Its a Rectangle");
+        }
+    }
 }
